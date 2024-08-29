7f72d578a9dbb9d20857d7c76273f779413e8747
Nicholas Liverett <liverettnicholas@gmail.com>
2024-08-29 17:32:06 -0500

    Initial commit

---
+++ Labs/Lab1/lab1.cpp
@@ -0,0 +1,20 @@
+/* 
+ Title:   lab1.cpp 
+ Author:  Nicholas Liverett
+ Date:  August 29, 2024 
+ Purpose: Convert Celsius to Fahrenheit 
+*/ 
+ 
+#include <iostream> 
+using namespace std; 
+ 
+int main() 
+{ 
+ double fahrenheit, celsius; 
+ cout << "\n\nWhat is the temperature in Celsius?  "; 
+ cin >> celsius; 
+ fahrenheit = celsius * (9.0/5.0) + 32; 
+ cout << "\nRESULT:  " << celsius << " degrees Celsius is "; 
+      cout << fahrenheit << " degrees Fahrenheit.\n\n"; 
+ return 0; 
+} 
