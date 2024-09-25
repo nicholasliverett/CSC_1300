edaec43a761942ba21892dc5c66265f739186407
Nicholas Liverett <liverettnicholas@gmail.com>
2024-09-25 09:00:33 -0500

    program1 fix

--- Programs/Program1/nliverett42_prog1.cpp
+++ Programs/Program1/nliverett42_prog1.cpp
@@ -1,7 +1,7 @@
 /* 
-    Title:   nliverett42_prog.cpp 
+    Title:   nliverett42_prog1.cpp 
     Author:  Nicholas Liverett
-    Date:  September 10, 2024 
+    Date:    September 25, 2024 
     Purpose: Create a simple game that tracks saucers and their speed
 */ 
 #include <iostream>
@@ -12,9 +12,9 @@ using namespace std;
 int main()
 {
     // Display game title
-    cout << endl << endl << "****************************" << endl;
+    cout << endl << endl << "****************************" << endl; // I don't like setw or fill
     cout << "*  ALIEN BLASTER SHOWDOWN  *" << endl;
-    cout << "****************************" << endl << endl;
+    cout << "****************************" << endl << endl; // sue me
 
     // Enter Game Master name
     string game_master_name;
@@ -22,55 +22,49 @@ int main()
     getline(cin, game_master_name);
     cout << endl << endl << "Welcome, Game Master " << game_master_name << ", to the Alien Blaster Showdown!" << endl;
 
-    // Loop vars setup
-    char saucer_incoming;
-    int saucer_distance, saucer_speed, i = 0, saucers_blasted = 0;
+    // Vars setup for loop, intercept calculations, and y/n input
+    char saucer_incoming = 'y';
+    int saucer_distance, saucer_speed, tracked_saucers = 0, saucers_blasted = 0;
     double saucer_intercept_time;
 
-    // Loop 10 times for shoot saucer game 
-    while (i < 10) {
+    // Loop until player inputs 'n'
+    while (saucer_incoming == 'y' || saucer_incoming == 'Y') {
+
+        // Get distance and speed to calculate saucer intercept time
         cout << "Enter the distance of the alien saucer in meters: ";
         cin >> saucer_distance;
         cout << "Enter the speed of the alien saucer in meters per second: ";
         cin >> saucer_speed;
-        saucer_intercept_time = static_cast<double>(saucer_distance)/saucer_speed; // this works
+        saucer_intercept_time = static_cast<double>(saucer_distance)/saucer_speed; // love me some static_cast
         cout << "Alien saucer detected! Time until it reaches the crowd: " << fixed << setprecision(2) << saucer_intercept_time << " seconds." << endl;
         
         // Change output based on intercept time to audience.
-        if (saucer_intercept_time <= 5) {
+        if (saucer_intercept_time < 5) {
             cout << "Blast it with foam cannons! It's zooming in too fast!";
             saucers_blasted++;
         } else if (saucer_intercept_time <= 15) {
             cout << "Hold fire! Let's cheer for the saucer as it zooms by!";
         } else if (saucer_intercept_time > 15) {
-            cout << "No need to worry-it's just hovering around. Keep enjoying the show!";
+            cout << "No need to worry, it's just hovering around. Keep enjoying the show!";
         }
         
-        // End while loop if user answers n, otherwise continue as normal
-        cout << endl << endl << "Is there another saucing approaching? (y/n): ";
+        // Query player for input y/n to continue shooting or not
+        cout << endl << endl << "Is there another saucer approaching? (y/n): ";
         cin >> saucer_incoming;
-        if (saucer_incoming == 'y') {
-
-            // Break while loop if 10 saucers have been tracked
-            if (i == 9) {
-                break; 
-            }
-        } else if (saucer_incoming == 'n') {
-            cout << endl;
-            cout << "The Alien Blaster Showdown has ended! You tracked " << i+1 << " saucers." << endl;
-            break;
-        }
-
-
-        i++;
+        cout << endl;
+        
+        tracked_saucers++;
     }
 
-    cout << "You blasted " << saucers_blasted << " saucers out of the air! Great job!" << endl;
+    // Output end of game stats
+    cout << "The Alien Blaster Showdown has ended! You tracked " << tracked_saucers << " saucers." << endl;
+    cout << "You blasted " << saucers_blasted << " saucer(s) out of the air! Great job!" << endl;
+
     // Change end message based on amount of blasted saucers
-    if (saucers_blasted < 7) {
-        cout << "What a peaceful show! Everyone left with smiles on their faces." << endl;
-    } else {
+    if (saucers_blasted > 7) {
         cout << "That was intense! The crowd loved it-10/10 entertainment!" << endl;
+    } else {
+        cout << "What a peaceful show! Everyone left with smiles on their faces." << endl;
     }
     
     return 0;
