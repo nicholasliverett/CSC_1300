f8f27e12d5e4f7fff62afbc480cd5778bed439b8
Nicholas Liverett <liverettnicholas@gmail.com>
2024-10-04 13:00:07 -0500

    mod6
    Prof changed reqs, what a turd

--- In-Class_Practices/mod6/mod6d.cpp
+++ In-Class_Practices/mod6/mod6d.cpp
@@ -24,23 +24,18 @@ int main()
         cout << "Sorry, " << file_name << " was unable to be opened.";
         return 1;
     }
-    while(getline(movie_file,entry, '%')) { // Pretty sure instructions are supposed to say delimit by '%' as there is no other characters in the file
-
-        // Get title line from entry
-        first = entry.find('\n');
-        title = entry.substr(0, first);
-        first++;
-
-        // Get year line from entry by starting find at the end of the title line
-        second = entry.find('\n', first);
-        year = entry.substr(first, second - first); // start at first and only go for 4 lines
-
-        // Same logic here, get rating line from entry by starting find at the end of the year line
-        rating = entry.substr(second+1);
+    while(getline(movie_file,entry, '#')) { 
+        if (i % 3 == 1)
+            title = entry; // Could getline() 2 more times instead of this
+        if (i % 3 == 2)
+            year = entry; // I'm a rebel >:)
+        if (i % 3 == 0)
+            rating = entry;
 
         // Output movie data
-        cout << " " << i << ") Title: " << title << ", Year: " << year << ", Rotten Tomatoes Score: " << rating << "%\n";
-        movie_file.ignore();
+        if (i % 3 == 0)
+            cout << " " << i/3 << ") Title: " << title << ", Year: " << year << ", Rotten Tomatoes Score: " << rating << "\n";
+        //movie_file.ignore();
         i++;
     }
     cout << endl;
@@ -49,37 +44,3 @@ int main()
 
     return 0; 
 } 
-
-// Easier way to do using istringstream - don't know if can use, did not see in lecture - like so much easier
-
-// #include <iostream>
-// #include <fstream>
-// #include <string>
-// #include <sstream>
-// using namespace std; 
- 
-// int main() 
-// { 
-//     string file_name, entry, title, year, rating;
-//     int i = 1;
-
-//     cout << "What is the filename? (example.txt): ";
-//     cin >> file_name;
-
-//     ifstream movie_file (file_name);
-//     if (!movie_file.is_open()) {
-//         cout << "Sorry, " << file_name << " was unable to be opened.";
-//         return 1;
-//     }
-//     while(getline(movie_file,entry, '%')) {
-
-//         istringstream entry_stream(entry);
-
-//         getline(entry_stream, title);
-//         getline(entry_stream, year);
-//         getline(entry_stream, rating);
-//         cout << " " << i << ") Title: " << title << ", Year: " << year << ", Rotten Tomatoes Score: " << rating << "%\n";
-//         movie_file.ignore();
-//         i++;
-//     }
-//     cout << endl;
